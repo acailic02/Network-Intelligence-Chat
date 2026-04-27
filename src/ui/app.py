00402365cd8fb723e29448353d5e188d85ee06f0
petar <petar1pa1990@gmail.com>
2026-04-27 21:22:29 +0200

    Added better UI.

--- src/ui/app.py
+++ src/ui/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from sqlalchemy.orm import Session
 from src.llm.client import chat
-import time
 from src.storage.db import engine
 from src.storage.repo import get_connections
 
@@ -16,20 +15,19 @@ def systemRes():
     data = request.get_json()
     userMSG = data["message"]
 
-    seconds = 2
-    time.sleep(seconds)
-
     res = chat(
         messages=[{"role": "user", "content": userMSG}],
-        system="Ti si asistent koji pomaže korisnicima da pretražuju svoju LinkedIn mrežu."
-    )["text"] #Here we call LLM
+        system="You are assistent that helps in searching users LinkedIn network."
+    )["text"]
 
     with Session(engine) as session:
         query_res = get_connections(session, country=userMSG)
 
+    profiles = [x.first_name + " " + x.last_name for x in query_res]
+
     return jsonify({
         "systemRes": res,
-        "profiles": [x.first_name + " " + x.last_name for x in query_res]
+        "profiles": profiles
     })
 
 if __name__ == "__main__":
